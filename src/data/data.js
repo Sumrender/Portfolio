74b14cc5bee719eec8d79b33b67716c669b737b0
Sumrender Singh <78428003+sumrender@users.noreply.github.com>
2023-05-01 05:50:55 +0530

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -2,7 +2,7 @@ export const links = {
   linkedin: "https://www.linkedin.com/in/sumrendersingh/",
   github: "https://github.com/sumrender",
   resume:
-    "https://drive.google.com/file/d/1nloIwHwxYrwPYLKjqRIckvIEJtj9Mpwk/view?usp=share_link",
+    "https://drive.google.com/file/d/1PQRklkrDvJoHyPugA56W-R7f2UBeGqtA/view?usp=share_link",
   twitter: "https://twitter.com/sumrenders",
 };
 export const projects = [
